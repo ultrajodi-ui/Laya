4d9e31f35a29f3c485f92821510a43ba06f58f6b
Ultrajodi <ultrajodi@gmail.com>
2025-09-08 11:19:41 +0000

    once i closed this workplace, and again i open this workplace but each t

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, initializeAuth, browserLocalPersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
@@ -15,7 +16,9 @@ const firebaseConfig = {
 
 // Initialize Firebase for client-side
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+const auth = initializeAuth(app, {
+    persistence: browserLocalPersistence
+});
 const db = getFirestore(app);
 
 export { app, auth, db };
