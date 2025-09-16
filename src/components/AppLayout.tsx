81665f05f128588a0831ed82bcd5b4f535d58b58
Ultrajodi <ultrajodi@gmail.com>
2025-09-16 06:39:35 +0000

    change browse page background as #f1fcff

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -329,7 +329,7 @@ function AppLayoutContent({ children }: { children: ReactNode }) {
                     </DropdownMenu>
                 </div>
             </header>
-            <main className="flex-1 p-4 sm:p-6" style={{ background: '#fff6fd' }}>{children}</main>
+            <main className="flex-1 p-4 sm:p-6" style={{ background: '#f1fcff' }}>{children}</main>
         </SidebarInset>
       </div>
     </SidebarProvider>
