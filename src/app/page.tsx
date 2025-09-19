9d14c894b02fd050074ab5f87af65f13152908de
Ultrajodi <ultrajodi@gmail.com>
2025-09-19 07:09:07 +0000

    change text alignment as center (_for element p_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,7 +136,7 @@ export default function Home() {
         </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground text-center sm:text-left">Ultra Jodi Matrimony is a trusted matrimonial service designed for those seeking a lifelong marriage. This is not a dating website. All profiles should reflect genuine marriage intentions. Copyright © 2025 Ultra Jodi Matrimony. All rights reserved.</p>
+        <p className="text-xs text-muted-foreground text-center">Ultra Jodi Matrimony is a trusted matrimonial service designed for those seeking a lifelong marriage. This is not a dating website. All profiles should reflect genuine marriage intentions. Copyright © 2025 Ultra Jodi Matrimony. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         </nav>
       </footer>
