d420dd6c5ccfa4a7068b6a7c982163d178ffeded
Ultrajodi <ultrajodi@gmail.com>
2025-09-19 07:06:32 +0000

    set background image src is "gs://ultra-jodi-wxhoi.firebasestorage.app/p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -138,9 +138,6 @@ export default function Home() {
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-muted-foreground text-center sm:text-left">Ultra Jodi Matrimony is a trusted matrimonial service designed for those seeking a lifelong marriage. This is not a dating website. All profiles should reflect genuine marriage intentions. Copyright © 2025 Ultra Jodi Matrimony. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
-            Privacy
-          </Link>
         </nav>
       </footer>
     </div>
