75d1f274f86e7954141995688c4d1483fb063d8a
Ultrajodi <ultrajodi@gmail.com>
2025-10-08 10:56:58 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,17 +6,26 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
+//const firebaseConfig = {
+//  "projectId": "ultra-jodi-wxhoi",
+//  "appId": "1:919030326656:web:575ef44228abd511350d65",
+ // "storageBucket": "ultra-jodi-wxhoi.firebasestorage.app",
+  //"apiKey": "AIzaSyDQyJmUDiYXIRaRn-Q7ByVUpVSD8jGBMRE",
+  //"authDomain": "ultra-jodi-wxhoi.firebaseapp.com",
+  //"messagingSenderId": "919030326656"
+//};
 const firebaseConfig = {
-  "projectId": "ultra-jodi-wxhoi",
-  "appId": "1:919030326656:web:575ef44228abd511350d65",
-  "storageBucket": "ultra-jodi-wxhoi.firebasestorage.app",
-  "apiKey": "AIzaSyDQyJmUDiYXIRaRn-Q7ByVUpVSD8jGBMRE",
-  "authDomain": "ultra-jodi-wxhoi.firebaseapp.com",
-  "messagingSenderId": "919030326656"
+  apiKey: "AIzaSyDQyJmUDiYXIRaRn-Q7ByVUpVSD8jGBMRE",
+  authDomain: "ultra-jodi-wxhoi.firebaseapp.com",
+  databaseURL: "https://ultra-jodi-wxhoi-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ultra-jodi-wxhoi",
+  storageBucket: "ultra-jodi-wxhoi.firebasestorage.app",
+  messagingSenderId: "919030326656",
+  appId: "1:919030326656:web:3805fc12c3a29d30350d65"
 };
-
+const app = initializeApp(firebaseConfig);
 // Initialize Firebase for client-side
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+//const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // Use initializeAuth for robust persistence
 const auth = initializeAuth(app, {
